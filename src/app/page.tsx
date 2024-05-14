f47a5684b27fdafde6d51a7d5141f04b783e43fa
mofarag107 <mo.farag.dev@gmail.com>
2024-05-14 14:14:38 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,20 @@ import { headers } from "next/headers";
 import { Card } from "./_components/Card";
 
 export default async function Home() {
-  const response = await fetch(
-    "https://server-test-bkja.onrender.com/api/v1/stores/host",
-    {
-      cache: "no-cache",
-    }
-  );
-
   const headersList = headers();
   const domain = headersList.get("x-forwarded-host") || "";
   const protocol = headersList.get("x-forwarded-proto") || "";
   const pathname = headersList.get("x-invoke-path") || "";
+  const response = await fetch(
+    "https://server-test-bkja.onrender.com/api/v1/stores/host",
+    {
+      cache: "no-cache",
+      headers: {
+        domain: domain,
+      },
+    }
+  );
+
   return (
     <>
       <h4>
