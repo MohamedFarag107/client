4bc5d27b3f80752867bd3e82e2ce5859efcd1258
mofarag107 <mo.farag.dev@gmail.com>
2024-05-14 14:18:28 +0300

    first commit

--- src/app/_components/Card.tsx
+++ src/app/_components/Card.tsx
@@ -1,12 +1,46 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const Card = () => {
   const location = window.location;
+  const [store, setStore] = useState<any>({});
+  const [status, setStatus] = useState<
+    "idle" | "loading" | "error" | "success"
+  >("idle");
+
+  useEffect(() => {
+    (async () => {
+      setStatus("loading");
+
+      const response = await fetch(
+        "https://server-test-bkja.onrender.com/api/v1/stores/host",
+        {
+          cache: "no-cache",
+          headers: {
+            domain: location.hostname,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        return setStatus("error");
+      }
+
+      const data = await response.json();
+      setStore(data);
+      setStatus("success");
+    })();
+  }, []);
   return (
     <div>
+      <h4>{status}</h4>
+      <h1>location</h1>
       <h4>
         <pre>{JSON.stringify(location, null, 2)}</pre>
+      </h4>
+      <h1>store</h1>
+      <h4>
+        <pre>{JSON.stringify(store, null, 2)}</pre>
       </h4>
     </div>
   );
